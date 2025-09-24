207372374b836ba15bbe7241a34c53ccbd1bf031
jEmzn <jirayu.nokkaew@gmail.com>
2025-09-25 01:25:04 +0700

    hashing linear, quadratic

---
+++ hashing/main.py
@@ -0,0 +1,22 @@
+from hash_table import HashTable
+from hash_table_qp import HashTableQP
+from hash_table_ch import HashTableCh
+
+
+h = HashTableQP(11)
+
+h[10] = "cat"
+h[22] = "dog"
+h[31] = "lion"
+h[4] = "cow"
+h[15] = "bird"
+h[28] = "goat"
+h[17] = "tiger"
+h[88] = "pig"
+h[59] = "fish"
+
+print(h.slots)
+print(9/11)
+
+
+# print(h.data)
